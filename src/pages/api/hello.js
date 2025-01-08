5abc49352e37ac1598a79b2d11e5981c828b1faf
salman <truesalman7@gmail.com>
2025-01-08 21:43:44 +0500

    log

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -2,5 +2,7 @@ import dbConnect from "../../../lib/dbConnect";
 
 export default async function handler(req, res) {
   await dbConnect();
+  console.log(dbConnect);
+
   res.status(200).json({ message: "Database Connected" });
 }
