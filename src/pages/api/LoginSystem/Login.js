3f74949c0ad582e1daa4f27c3818326ccece002c
ammarrahim555 <ammarrahim555@gmail.com>
2025-01-23 21:40:06 +0500

    minor change

--- src/pages/api/LoginSystem/Login.js
+++ src/pages/api/LoginSystem/Login.js
@@ -22,15 +22,16 @@ export default async function handler(req, res) {
       if (!user) {
         return res.status(404).json({ message: "User not found" });
       }
-
+      console.log(user.role);
+      
       
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
         return res.status(401).json({ message: "Invalid credentials" });
       }
 
-     
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      
+      const token = jwt.sign({ id: user._id  , role : user.role }, process.env.JWT_SECRET, {
         expiresIn: "1d",
       });
 
