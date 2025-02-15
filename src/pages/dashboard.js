5564ea644924beee06e5ecfa42c88bf2161d6906
salman <truesalman7@gmail.com>
2025-02-15 09:10:50 +0500

    dashboard line delete

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,5 @@
 // pages/dashboard.js
 import { Box, Typography } from "@mui/material";
-import Sidebar from "../../component/SideBar/SideBar";
-import TnaChart from "../../component/TnaTarget/TnaTarget";
-import LineWiseTarget from "../../component/LineWiseTarget/LineWiseTarget";
 
 const DashboardPage = () => {
   return (
@@ -23,17 +20,6 @@ const DashboardPage = () => {
           Dashboard
         </Typography>
         <Typography variant="h6">Welcome to the Dashboard</Typography>
-
-        {/* TNA Chart */}
-        <Box mb={3}>
-          <TnaChart />
-        </Box>
-
-        {/* LineWise Target Chart */}
-        <Box>
-          <LineWiseTarget />
-          {/* <LineWiseTarget /> */}
-        </Box>
       </Box>
     </Box>
   );
